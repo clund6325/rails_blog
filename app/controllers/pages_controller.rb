42d7c3f4aa60118e888892f4cae24eec79dbaa0c
Cameron Lund <clund6314@gmail.com>
2021-05-24 20:02:18 -0600

    models and database stuff

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def index
+    @pages = Page.all
+    render component: 'Pages', props:{ pages: @pages }
+  end
+
+  def show
+    @page = Page.find(params[:id])
+    render component: 'Page', props:{page: @page}
+  end
+
+  def new
+    @page = Page.new
+    render component: 'PageNew', props:{ page: @page}
+  end
+end
